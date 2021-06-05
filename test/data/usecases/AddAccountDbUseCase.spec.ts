189b7f899ff2d2cc0611ef7f78660c359ddd5fa4
Zaqueu Santos <zaqueulima1@gmail.com>
2021-06-05 17:05:08 -0300

    test: ensure AddAccountDbUseCase return an account on success

--- test/data/usecases/AddAccountDbUseCase.spec.ts
+++ test/data/usecases/AddAccountDbUseCase.spec.ts
@@ -101,4 +101,20 @@ describe('AddAccountDbUseCase', () => {
     const promise = sut.execute(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const account = await sut.execute(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'encrypted_password'
+    })
+  })
 })
