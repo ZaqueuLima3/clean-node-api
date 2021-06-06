54de3834c6eb2e9a0e629c743c736754fce650e1
Zaqueu Santos <zaqueulima1@gmail.com>
2021-06-06 09:46:47 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ test/infra/database/mongodb/account-repository/AccountRepository.spec.ts
@@ -0,0 +1,26 @@
+import { AccountMongoRepository } from '@/infra/database/mongodb/account-repository/AccountMongoRepository'
+import { MongoHelper } from '@/infra/database/mongodb/helpers/MongoHelper'
+
+describe('AccountRepository Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('any_password')
+  })
+})
